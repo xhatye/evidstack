b9c8ddd9e9ead1900134d059db606abc288ede7b
xhatye <eythax.eythube@gmail.com>
2026-09-11 16:57:40 +0200

    Add Body Atlas interactive evidence map

---
+++ src/BodyAtlasHooks.js
@@ -0,0 +1,12 @@
+import { useEffect, useState } from "react";
+
+export function useIsMobile(breakpoint = 768) {
+  const [isMobile, setIsMobile] = useState(() => typeof window !== "undefined" && window.innerWidth < breakpoint);
+  useEffect(() => {
+    const onResize = () => setIsMobile(window.innerWidth < breakpoint);
+    window.addEventListener("resize", onResize);
+    return () => window.removeEventListener("resize", onResize);
+  }, [breakpoint]);
+  return isMobile;
+}
+
